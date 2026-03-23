2a2d62822f66a9023949fa36277e5ae7a5de8353
mandre <marcosandredev25@gmail.com>
2026-03-22 23:33:34 -0300

    style: add Exo 2 and Rajdhani fonts for modern tech aesthetic

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ['"Exo 2"', 'sans-serif'],
+        body: ['Rajdhani', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
